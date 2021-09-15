103567c56a2adaecb479bd9a461f6bead0d1b773
Target52 <gulien@me.com>
2021-09-15 11:04:45 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    if (i % 2 == 0) {
+        console.log("The number is even.");
+    } else {
+        console.log("The number is odd.");
+    }
+}
---
+++ multiplication-tables.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 10; i++) {
+    console.log('The table of ' + i + ':');
+    for (let p = 1; p <= 10; p++) {
+        console.log(p + ' x ' + i + ' = ' + p * i);
+    }
+    console.log('\n');
+}
